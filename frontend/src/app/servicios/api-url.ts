6ed2e488925dd214b0adce048129b60afbc6bfa7
c3r38r170 <c3r38r170@gmail.com>
2023-12-12 10:18:24 -0300

    URL de railway en el frontend.

--- frontend/src/app/servicios/api-url.ts
+++ frontend/src/app/servicios/api-url.ts
@@ -1,2 +1,1 @@
-// export const API_URL='https://ttads-fullstack.onrender.com/api/';
-export const API_URL='http://localhost:8080/api/';
+export const API_URL='https://tp-fullstack-production.up.railway.app/api/';
